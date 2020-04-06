05e7ea8a0b42b869957f932d6b7f3dae8275bc50
Itay Ganor <6430313+itayganor@users.noreply.github.com>
2020-04-06 12:52:46 +0300

    Initial Commit

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+export default function useTextFit(): {
+    ref: import("react").MutableRefObject<HTMLElement>;
+};
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import {useEffect, useRef} from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+
+
+export default function useTextFit() {
+    const ref = useRef<HTMLElement>(null);
+
+    const resizeObserver = new ResizeObserver(() => {
+        reCalculate();
+    });
+
+    useEffect(() => {
+        if (!ref.current) {
+            return;
+        }
+
+        ref.current.style.display = 'inline-block'; // so widths calculation fit text and not container
+        reCalculate();
+
+        resizeObserver.observe(ref.current);
+        return () => resizeObserver.unobserve(ref.current);
+    }, [ref]);
+
+    function reCalculate() {
+        ref.current.style.fontSize = '';
+
+        const text = ref.current.innerText;
+        const measureDiv = document.createElement('div');
+
+        ref.current.appendChild(measureDiv);
+        measureDiv.style.whiteSpace = 'pre';
+        measureDiv.style.position = 'fixed';
+        measureDiv.style.display = 'inline-block';
+        measureDiv.style.top = '-9999px';
+        measureDiv.style.opacity = '0';
+        measureDiv.style.pointerEvents = 'none';
+        measureDiv.innerText = text;
+
+        const diff = getInlineSize(ref.current) / (getInlineSize(measureDiv) + 1);
+
+        ref.current.style.fontSize = Math.min(diff, 1) * 100 + '%';
+
+        measureDiv.remove();
+    }
+
+    return {ref};
+}
+
+function getInlineSize(el: HTMLElement) {
+    return isWritingModeSideways(el) ? el.offsetHeight : el.offsetWidth;
+    // return parseInt(getComputedStyle(el).inlineSize, 10);
+}
+
+function isWritingModeSideways(el: HTMLElement) {
+    return ['vertical-rl', 'vertical-lr', 'tb', 'tb-rl', 'sideways-rl', 'sideways-lr']
+        .includes(getComputedStyle(el).writingMode);
+}
